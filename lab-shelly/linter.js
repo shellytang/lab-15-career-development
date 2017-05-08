11ae4bd8e32c2e80f47aa8c18581d8d9148d0f31
Shelly Tang <shellytang@gmail.com>
2017-05-08 12:37:20 -0700

    tests

--- lab-shelly/linter.js
+++ lab-shelly/linter.js
@@ -1,31 +1,36 @@
 'use strict';
 
-const Stack = require('./lib/stack');
-let openerStack = new Stack;
+//O(n) notation
 
+const Stack = require('./lib/stack');
+let openerStack = new Stack();
+// let testString = '[]{}()';
 const Linter = module.exports = function(string) {
 
   for(let i = 0; i <string.length; i ++) {
-
     if(string.charAt(i) === '[' || string.charAt(i) === '{' ||  string.charAt(i) === '(') {
       openerStack.push(string[i]);
     }
-    if(string.charAt(i) === ']' || string.charAt(i) === '}' || string.charAt(i) === ')') {
 
+    if(string.charAt(i) === ']' || string.charAt(i) === '}' || string.charAt(i) === ')') {
       if(openerStack.head === null) {
-        throw new Error('Missing matching opener');
+        // console.error('Missing matching opener');
+        return false;
       }
 
       let current = string.charAt(i);
       let top = openerStack.peek();
-
       if(top === '[' && current === ']' || top === '{' && current === '}' || top === '(' && current === ')') {
         openerStack.pop();
       }
     }
   }
+
   if(openerStack.head !== null) {
-    console.log('what is itttt', openerStack.head);
-    throw new Error('Missing matching closer');
+    // console.error('Missing matching closer');
+    return false;
   }
+  // console.log('success');
+  return true;
 };
+// Linter(testString);
